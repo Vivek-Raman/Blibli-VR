7002810c1f238a73c3b7e5eccd59c7e1c4d11d19
Vivek Raman <vr.ac4bf@live.com>
2022-12-18 20:17:51 +0530

    Add 3P Assets to gitignore, Restructed Catalog, Items, and Spaces

---
+++ Assets/Scripts/Quinbay/Catalog/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Quinbay.Catalog.Data;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] protected CatalogItem catalogItem;
+
+
+}
